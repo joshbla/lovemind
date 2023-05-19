db7a377f1199a1a3cb5f6545dfef2e6ed62ef0ad
Joshua Blake Smith <jblakezsmith@gmail.com>
2023-05-19 16:08:52 -0600

    Creation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-blue-500 text-white text-center font-bold p-10">
+      Welcome to your new React TypeScript Tailwind website!
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
